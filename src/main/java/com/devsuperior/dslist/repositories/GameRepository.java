a4cccb4466926086aebfe844e876f4a15c5b564f
Lucas Paim <lucaspaimrj21@gmail.com>
2025-02-07 20:17:27 -0300

    move games on list

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -14,7 +14,7 @@ public interface GameRepository extends JpaRepository<Game, Long> {
 			SELECT tb_game.id, tb_game.title, tb_game.game_year AS "year", tb_game.img_url AS imgUrl, 
 			tb_game.short_description AS shortDescription, tb_belonging.position
 			FROM tb_game
-			INNER JOIN stb_belonging ON tb_game.id = tb_belonging.game_id
+			INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
 			WHERE tb_belonging.list_id = :listId
 			ORDER BY tb_belonging.position
 				""")
